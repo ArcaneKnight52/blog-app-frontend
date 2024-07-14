03ed5cc884b3408beb0003e48e695aa35b6b608f
ArcaneKnight52 <athithian1643@gmail.com>
2024-07-14 22:03:08 +0530

    added login page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import './App.css';
+import { BrowserRouter as Router,Route,Routes } from 'react-router-dom';
+import Login from './pages/Login';
+
 
 function App() {
   return (
-    <div className="App">
-      <p className='pt-5'>hi</p>
-    </div>
+    <Router>
+      <div className="app">
+        <Routes>
+          <Route exact path="/" Component={Login}/>
+          {/* <Route path="/signup" Component={SignUp} /> */}
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
